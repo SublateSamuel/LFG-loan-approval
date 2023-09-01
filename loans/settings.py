3d04321a15675f6589e0d2b2a12d2ea1f359b0de
Samuel Sublate <87145442+SublateSamuel@users.noreply.github.com>
2023-09-01 14:50:40 -0300

    build: Configurar pipeline de testes com GitHub actions (#23)
    * refactor: adjust style code
    
    * build: add pipeline in github actions
    
    * build: remove include path
    
    * test: add test redirec http admin page

--- loans/settings.py
+++ loans/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-um+*9(uxtnr_jx@47d65!d*i+uu)xs59773gis1s^22z*-9b6-'
+SECRET_KEY = (
+    'django-insecure-um+*9(uxtnr_jx@47d65!d*i+uu)xs59773gis1s^22z*-9b6-'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
